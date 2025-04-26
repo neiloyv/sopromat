03dda4949f85958f3baf891e8571ca14c5fd9ee5
neiloyv <neiloyv@gmail.com>
2025-04-26 10:12:16 +0300

    First commit

---
+++ src/main/java/com/neilo/sopromat/SopromatApplication.java
@@ -0,0 +1,13 @@
+package com.neilo.sopromat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopromatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SopromatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neilo/sopromat/SopromatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neilo.sopromat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopromatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
