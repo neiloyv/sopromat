29d6a04e20d4f9548906e4b5dbe8332c80ea45c5
neiloyv <neiloyv@gmail.com>
2025-05-10 14:23:55 +0300

    sopromat-4-calc-ra-rb

--- src/main/java/com/neilo/sopromat/service/HingeBeamServiceImpl.java
+++ src/main/java/com/neilo/sopromat/service/HingeBeamServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Map;
+import java.util.TreeMap;
 
 @Service
 @Slf4j
@@ -20,24 +22,61 @@ public class HingeBeamServiceImpl implements HingeBeamService {
         BigDecimal xq1 = hingeBeamDto.getDistributedForce().getPositionStart();
         BigDecimal xq2 = hingeBeamDto.getDistributedForce().getPositionEnd();
         BigDecimal m = hingeBeamDto.getMoment().getMoment();
+        BigDecimal xm = hingeBeamDto.getMoment().getPosition();
         BigDecimal xa = hingeBeamDto.getPositionLeftBeamReaction();
         BigDecimal xb = hingeBeamDto.getPositionRightBeamReaction();
 
-        BigDecimal ra = (
-                f.multiply(xb.subtract(xf))
-                        .add(q.multiply((xq2.subtract(xq1)))
-                                .divide(xb.subtract((xq2.subtract(xq1)).divide(BigDecimal.TWO, 2, RoundingMode.HALF_UP)), 2, RoundingMode.HALF_UP))
-                        .add(m))
-                .divide(xb.subtract(xa), 2, RoundingMode.HALF_UP);
+        BigDecimal q1 = q.multiply((xq2.subtract(xq1)));
 
-        BigDecimal rb = (
-                f.multiply(xf.subtract(xa))
-                        .add(q.multiply((xq2.subtract(xq1)))
-                                .divide(xq1.add((xq2.subtract(xq1)).divide(BigDecimal.TWO, 2, RoundingMode.HALF_UP)), 2, RoundingMode.HALF_UP))
-                        .add(m)
-        )
-                .divide(xb.subtract(xa), 2, RoundingMode.HALF_UP);
+        BigDecimal mfa = f.multiply(xb.subtract(xf));
+        BigDecimal mqa = q1.multiply(xb.subtract(xq1.add((xq2.subtract(xq1)).divide(BigDecimal.TWO, 2, RoundingMode.HALF_UP))));
 
+        BigDecimal ra = (mfa.add(mqa).subtract(m)).divide(xb.subtract(xa), 2, RoundingMode.HALF_UP);
+
+        BigDecimal mfb = f.multiply(xf.subtract(xa));
+        BigDecimal mqb = q1.multiply(xq1.add((xq2.subtract(xq1)).divide(BigDecimal.TWO, 2, RoundingMode.HALF_UP)));
+
+        BigDecimal rb = (mfb.add(mqb).add(m)).divide(xb.subtract(xa), 2, RoundingMode.HALF_UP);
+
+        System.out.println("xf = " + xf);
+        System.out.println("q = " + q);
+        System.out.println("xq1 = " + xq1);
+        System.out.println("xq2 = " + xq2);
+        System.out.println("m = " + m);
+        System.out.println("xa = " + xa);
+        System.out.println("xb = " + xb);
+        System.out.println("====================");
+        System.out.println("mfa = " + mfa);
+        System.out.println("mqa = " + mqa);
+        System.out.println("====================");
+        System.out.println("mfb = " + mfb);
+        System.out.println("mqb = " + mqb);
+        System.out.println("====================");
+        System.out.println("ra = " + ra);
+        System.out.println("rb = " + rb);
+
+        Map<BigDecimal, BigDecimal> diagramForces = new TreeMap<>();
+        Map<BigDecimal, BigDecimal> diagramQ = new TreeMap<>();
+        Map<BigDecimal, BigDecimal> diagramM = new TreeMap<>();
+
+        diagramForces.put(xa, ra);
+        diagramForces.put(xf, f.negate());
+        diagramForces.put(xq1, q.negate());
+        diagramForces.put(xq1.add((xq2.subtract(xq1)).divide(BigDecimal.TWO, 2, RoundingMode.HALF_UP)), q.negate());
+        diagramForces.put(xq2, q.negate());
+        diagramForces.put(xm, m);
+        diagramForces.put(xb, rb);
+
+
+        diagramForces.forEach((position, force) -> {
+            double sumForces = diagramQ.values().stream().mapToDouble(BigDecimal::doubleValue).sum();
+            diagramQ.put(position, BigDecimal.valueOf(sumForces).add(force));
+        });
+
+        System.out.println("diagramForces: " + diagramForces);
+        System.out.println("diagramQ: " + diagramQ);
+
+//        diagramQ.put(xa, ra);
 
     }
 
